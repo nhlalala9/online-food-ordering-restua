791a751b4832a74dd40b077cdbc8b1390bcaf4f3
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-23 11:12:54 +0200

    iccons

--- online-food-ordering-restuarant/src/app/app-routing.module.ts
+++ online-food-ordering-restuarant/src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupGuard } from './guards/signup.guard';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 // import { LandingComponent } from './components/landing/landing.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   
-  { path: 'restuarant/login', component: LoginComponent },
+  { path: 'restaurant/login', component: LoginComponent },
   {
-    path: 'restuarant/register',
+    path: 'restaurant/register',
     component: RegisterComponent,
   },
+  {path: 'restaurant/sidebar',component:SidebarComponent},
 ];
 
 @NgModule({
