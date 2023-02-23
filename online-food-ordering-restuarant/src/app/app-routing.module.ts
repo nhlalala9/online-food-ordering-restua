1293abdddbec7e5b89af409499c76bbdacab5b6e
Oratile1234 <oratilemabote16@gmail.com>
2023-02-23 16:06:53 +0200

    dash

--- online-food-ordering-restuarant/src/app/app-routing.module.ts
+++ online-food-ordering-restuarant/src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupGuard } from './guards/signup.guard';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-// import { LandingComponent } from './components/landing/landing.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
+
+
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  
+  { path: 'restuarant/dashboard', component: DashboardComponent },
   { path: 'restuarant/login', component: LoginComponent },
   {
     path: 'restuarant/register',
     component: RegisterComponent,
   },
+  { path: 'restuarnt/products', component: ProductsComponent },
 ];
 
 @NgModule({
--- online-food-ordering-restuarant/src/app/app.module.ts
+++ online-food-ordering-restuarant/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
-    
+import { MatCardModule } from '@angular/material/card'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,21 +9,32 @@ import { RegisterComponent } from './components/register/register.component';
 import { ToastComponent } from './components/toast/toast.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-// import { LandingComponent } from './components/landing/landing.component';
-@NgModule({
+import { ProductsComponent } from './components/products/products.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+@NgModule({  
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
     ToastComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProductsComponent,
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
