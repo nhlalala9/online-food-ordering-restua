68b06fb0d9128175056eaedc1b9268bc093ea9a5
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-27 14:02:13 +0200

    products

---
+++ online-food-ordering-restuarant/src/app/service/products.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  apiUrl = 'http://localhost:1337';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/api/products?populate=*`);
+  }
+  createProducts(product: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/api/products`, product);
+  }
+
+  updateProducts(product: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/api/products/${product.id}`, product);
+  }
+
+  deleteProducts(productId: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/api/products/${productId}`);
+  }
+
+
+}
