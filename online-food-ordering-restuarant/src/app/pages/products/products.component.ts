c20f3ceafdcac567249f93435013174b65b21cfb
ignatius <ignatius.mafahla@dadevs.co.za>
2023-02-28 08:33:36 +0200

    products

--- online-food-ordering-restuarant/src/app/pages/products/products.component.ts
+++ online-food-ordering-restuarant/src/app/pages/products/products.component.ts
@@ -17,55 +17,17 @@ export class ProductsComponent implements OnInit {
   constructor(private ProductService: ProductsService) { }
 
   ngOnInit(): void {
-    this.getAllMyModels();
+    // this.getAllMyModels();
   }
-  getAllMyModels(): void {
-    this.ProductService.getAll()
-      .subscribe(models => {
-        this.myModels = models;
-      });
-  }
+  
 
-  getOneMyModel(id: number): void {
-    this.ProductService.getOne(id)
-      .subscribe(model => {
-        this.currentModel = model;
-      });
-  }
+ 
 
-  createMyModel(data: any): void {
-    this.ProductService.create(data)
-      .subscribe(() => {
-        this.isCreating = false;
-        this.getAllMyModels();
-      });
-  }
 
-  updateMyModel(id: number, data: any): void {
-    this.ProductService.update(id, data)
-      .subscribe(() => {
-        this.isUpdating = false;
-        this.currentModel = {};
-        this.getAllMyModels();
-      });
-  }
 
-  deleteMyModel(id: number): void {
-    this.ProductService.delete(id)
-      .subscribe(() => {
-        this.getAllMyModels();
-      });
-  }
+ 
 
-  toggleCreate(): void {
-    this.isCreating = !this.isCreating;
-  }
-
-  toggleUpdate(id: number): void {
-    this.isUpdating = !this.isUpdating;
-    if (id) {
-      this.getOneMyModel(id);
-    }
-  }
+  
+ 
 
 }
