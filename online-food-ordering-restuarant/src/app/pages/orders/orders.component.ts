e348d423fa8c4118c63873f95dee2990ee6ffd7e
Oratile1234 <oratilemabote16@gmail.com>
2023-03-06 00:20:03 +0200

    late

--- online-food-ordering-restuarant/src/app/pages/orders/orders.component.ts
+++ online-food-ordering-restuarant/src/app/pages/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersServiceService } from 'src/app/service/orders-service.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  orders: any[] = [];
+
+  constructor(private orderService: OrdersServiceService) { }
 
   ngOnInit(): void {
+    // this.orderService.getOrders().subscribe(
+    //   orders => {
+    //     this.orders = orders;
+    //     console.log(this.orders); // console log orders data here
+    //   },
+    //   error => console.log(error)
+    // );
+    this.orderService.getOrders().subscribe((booking: any) =>{
+      this.orders = booking.data;
+      console.log(this.orders)
+    })
   }
+  
 
 }
