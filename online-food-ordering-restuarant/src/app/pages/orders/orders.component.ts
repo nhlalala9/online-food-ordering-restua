966e68a927e8c2ea7872fff44754d70565c64632
Oratile1234 <oratilemabote16@gmail.com>
2023-03-06 00:47:39 +0200

    late

--- online-food-ordering-restuarant/src/app/pages/orders/orders.component.ts
+++ online-food-ordering-restuarant/src/app/pages/orders/orders.component.ts
@@ -9,6 +9,8 @@ import { OrdersServiceService } from 'src/app/service/orders-service.service';
 export class OrdersComponent implements OnInit {
 
   orders: any[] = [];
+  order:any
+  cartDetails: any[] = [];
 
   constructor(private orderService: OrdersServiceService) { }
 
@@ -23,6 +25,13 @@ export class OrdersComponent implements OnInit {
     this.orderService.getOrders().subscribe((booking: any) =>{
       this.orders = booking.data;
       console.log(this.orders)
+    })
+
+    const orderId = 2;
+    this.orderService.getOrderById(orderId).subscribe((booking: any) =>{
+      this.order = booking.data;
+      this.cartDetails = this.order.data.attributes.cartDetails;
+      console.log(this.cartDetails)
     })
   }
   
